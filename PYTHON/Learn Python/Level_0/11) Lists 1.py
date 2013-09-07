85d7ec1173421a5848ac681b09c4cd2408ba339b
bltsandwich1 <itcaughtonj@gmail.com>
2013-09-07 18:19:53 -0500

    Added Lists 1

---
+++ PYTHON/Learn Python/Level_0/11) Lists 1.py
@@ -0,0 +1,12 @@
+###
+#Introduction to lists
+###
+
+#Lists are a set of comma seperated values that
+#are usually of the same type, (although this
+#isn't always true)
+
+#Let's start off with an easy list
+
+Kindergarten = ['a', 'b', 'c', 1, 2, 3]
+print(Kindergarten)
---
+++ PYTHON/Learn Python/Level_0/Challenges/Solutions/11) Lists 1.py
@@ -0,0 +1,6 @@
+# Lists 1 solution
+
+list1 = [2, 3, 5, 7, 11]
+list2 = [1, 8, 27, 64, 125]
+
+print(list1, list2)
