d2a30cebb7495703a796a7e48bd55fcf41aa3e6d
bltsandwich1 <itcaughtonj@gmail.com>
2014-02-01 12:34:04 -0600

    Comment update

--- PYTHON/Learn Python/Level_0/4) Variable 2.py
+++ PYTHON/Learn Python/Level_0/4) Variable 2.py
@@ -12,6 +12,8 @@ x=54 #we are redefining x to equal 54
 print (x) 
 x= "Cheese" #x is now the string 'cheese'
 print (x)
+#Try running this program  to see x 
+#printed at each point
 
 #Clearly variables can be manipulated easily,
 #this can make them very useful
