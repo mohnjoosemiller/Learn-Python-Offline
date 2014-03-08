e4ab2c9811c4dff9cf290ed2bb7a29ccebaf76ae
bltsandwich1 <itcaughtonj@gmail.com>
2014-03-07 21:48:00 -0600

    Code updates

--- PYTHON/Learn Python/Level_1/1) Data Read-In 1.py
+++ PYTHON/Learn Python/Level_1/1) Data Read-In 1.py
@@ -6,8 +6,8 @@
 #Reading files is pretty easy.
 
 f = open('Sample_1.txt')#There can be more arguments than we have here
-#######################But for now we have opened Sample in read mode
-#######################Sample is in the same folder as this program
+#########################But for now we have opened Sample in read mode
+#########################Sample is in the same folder as this program
 
 print f.readline() #This will print the first line in the file
 print f.readline() #this will print the second line in the file
@@ -18,4 +18,4 @@ print f.read() #this will read the rest of the file
 
 #When you run this you will notice the gaps between lines, this is
 #because there is a new line read in the file, as well as a new line
-#printed as part of python.
+#printed as part of python in order to differentiate command outputs.
--- PYTHON/Learn Python/Level_1/2) Data Read-In 2.py
+++ PYTHON/Learn Python/Level_1/2) Data Read-In 2.py
@@ -5,17 +5,16 @@
 #often we want to control exactly what we read from
 #a file. Here we learn a tad more about reading
 
-#Loops come in handy especially when dealling with
+#Loops come in handy especially when dealing with
 #longer files
 
 #An example:
 
 f = open('Sample_2.txt', 'r')#last time we didn't include a
-##############################second argument, 'r' tells Pyhton
+##############################second argument, 'r' tells Python
 ##############################to open a file in read-only mode
 
-x=0
 for x in range(1, 9):
-    print f.readline()
-    x += 1
+    print f.readline()#reads the line that the 
+    x += 1 #to advance to the next value
     
